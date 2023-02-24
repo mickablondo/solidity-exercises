405a32745c583d254479abdcb91569d3afbcf3f6
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2023-02-24 21:30:26 +0100

    gestion des différents types

--- contracts/1_GetterSetter.sol
+++ contracts/1_GetterSetter.sol
@@ -2,13 +2,50 @@
 pragma solidity 0.8.19;
 
 contract testGetterSetter {
-    uint nombre;
+    // différents types
+    uint unsignedNombre;
+    int nombre;
+    bool estCeVrai;
+    address etherAddress;
+    string maPhrase;
 
-    function getNombre() public view returns(uint) {
+    function getUnsignedNombre() public view returns(uint) {
+        return unsignedNombre;
+    }
+
+    function setUnsignedNombre(uint _unsignedNombre) public {
+        unsignedNombre = _unsignedNombre;
+    }
+
+    function getNombre() public view returns(int) {
         return nombre;
     }
 
-    function setNombre(uint _nombre) public {
+    function setNombre(int _nombre) public {
         nombre = _nombre;
+    }
+    
+    function getEstCeVrai() public view returns(bool) {
+        return estCeVrai;
+    }
+
+    function setEstCeVrai(bool _estCeVrai) public {
+        estCeVrai = _estCeVrai;
+    }
+ 
+    function getEtherAddress() public view returns(address) {
+        return etherAddress;
+    }
+
+    function setEtherAddress(address _etherAddress) public {
+        etherAddress = _etherAddress;
+    }   
+    
+    function getMaPhrase() public view returns(string memory) {
+        return maPhrase;
+    }
+
+    function setMaPhrase(string memory _maPhrase) public {
+        maPhrase = _maPhrase;
     }
 }
