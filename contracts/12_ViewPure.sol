04af159e97c10e500408fa05cdafafd8e8b509ce
Micka Blondeau <36310658+mickablondo@users.noreply.github.com>
2023-03-29 18:16:15 +0200

    Exercise 12
    Ajout code

--- contracts/12_ViewPure.sol
+++ contracts/12_ViewPure.sol
@@ -3,17 +3,42 @@ pragma solidity 0.8.19;
 
 // @author MiK@
 contract testMotsCles {
-    // view : venir récupérer une valeur et NON la changer => getters par exemple
-    // pure : juste pour faire des calculs
 
-    // private : appel de la fonction depuis l'intérieur du contrat (norme : utiliser _ devant le nom de la fonction)
-    // private variable (défaut) : variable utilisable seulement depuis l'intérieur du contrat
+    /**
+        VARIABLES
+     */
+    uint private nombreSecret; // PRIVATE (valeur par défaut) : variable utilisable seulement depuis l'intérieur du contrat
+    uint public nombreAccessible; // PUBLIC variable :  variable utilisable depuis partout => crée une sorte de getter automatiquement
+    uint internal nombreSecretMaisPastrop; // INTERNAL variable :  variable utilisable seulement depuis l'intérieur du contrat et depuis les contrats dérivés
 
-    // internal : appel de la fonction depuis l'intérieur du contrat et depuis les contrats qui héritent de ce contrat (exemple : Owner)
-    // internal internal :  variable utilisable seulement depuis l'intérieur du contrat et depuis les contrats dérivés
+    /**
+        FONCTIONS
+     */
 
-    // external : appel de la fonction depuis l'extérieur du contrat seulement (et non pas depuis l'intérieur)
+    // PUBLIC : appel de la fonction depuis l'extérieur ou depuis l'intérieur
+    // VIEW : venir récupérer une valeur et NON la changer => getters par exemple
+    function getMyAddress() view public returns(address) {
+        return msg.sender;
+    }
 
-    // public : appel de la fonction depuis l'extérieur ou depuis l'intérieur
-    // public variable :  variable utilisable seulement depuis partour (exemple : uint public nombre) => crée une sorte de getter automatiquement
+    // PURE : juste pour faire des calculs
+    function fois2(uint _nombre) pure public returns(uint) {
+        return _nombre*2;
+    }
+
+
+    // PRIVATE : appel de la fonction depuis l'intérieur du contrat (norme : utiliser _ devant le nom de la fonction)
+    function _secreteFois2() private {
+        nombreSecret *= 2;
+    }
+
+    // INTERNAL : appel de la fonction depuis l'intérieur du contrat et depuis les contrats qui héritent de ce contrat (exemple : Owner)
+    function _presqueSecretFois2() internal {
+        nombreSecretMaisPastrop *= 2;
+    }
+
+    // EXTERNAL : appel de la fonction depuis l'extérieur du contrat seulement (et non pas depuis l'intérieur)
+    function paiement() external payable {
+        nombreAccessible = msg.value;
+    }
 }
